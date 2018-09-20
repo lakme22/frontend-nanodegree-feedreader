0128af08b8f984a89f547209783d61422b1692c6
lakme22 <lakmariappan1997@gmail.com>
2018-09-20 19:21:19 +0530

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,14 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('url is defined', function() {
+            allFeeds.forEach(function(feed) {
+            feedLink = feed.url;
+            expect(feedLink).toBeDefined();
+            expect(feedLink.length).not.toBe(0);
+          });
+       });
+    
 
 
         /* TODO: Write a test that loops through each feed
@@ -39,7 +47,14 @@ $(function() {
          */
     });
 
-
+    it('name is defined', function() {
+        allFeeds.forEach(function(feed) {
+        feedName = feed.name;
+        expect(feedName).toBeDefined();
+        expect(feedName.length).not.toBe(0);
+      });
+   });
+});
     /* TODO: Write a new test suite named "The menu" */
 
         /* TODO: Write a test that ensures the menu element is
@@ -53,7 +68,27 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+         describe("The menu", function() {
 
+            // Pre-define elements needed for testing hiding/showing of the menu
+            var body = document.body;
+            var menuIcon = document.querySelector(".menu-icon-link");
+        
+            // Make sure the menu is hidden initially
+            it("body has 'menu-hidden' initially", function() {
+              expect(body.className).toContain("menu-hidden");
+            });
+        
+            // Make sure menu icon toggles hide/show on clicking
+            it("body toggles the class 'menu-hidden' on clicking menu icon", function() {
+              menuIcon.click();
+              expect(body.className).not.toContain("menu-hidden");
+        
+              menuIcon.click();
+              expect(body.className).toContain("menu-hidden");
+            });
+          });
+        
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
@@ -62,6 +97,28 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        describe("Initial Entries", function() {
+
+            // Avoid duplicated setup
+            // Before loading feed
+            beforeEach(function(done) {
+              loadFeed(0, function() {
+                done();
+              });
+            });
+        it("has at least 1 entry after loadFeed function is called", function(done) {
+            var numEntries = document.querySelector(".feed").getElementsByClassName("entry").length;
+            expect(numEntries).toBeGreaterThan(0);
+            done();
+          });
+          it("has a entry that has a link starting with 'http(s)://'", function(done) {
+            var entries = document.querySelector(".feed").getElementsByClassName("entry-link");
+            for(var i = 0; i < entries.length; i++){
+              expect(entries[i].href).toMatch(/^(http|https):\/\//);
+            }
+            done();
+          });
+        });      
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
@@ -69,4 +126,24 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-}());
+        describe("New Feed Selection", function() {
+
+            // Avoid duplicated setup
+            // Initial loaded feed setup
+            var initFeedSelection;
+            beforeEach(function(done) {
+              loadFeed(0, function() {
+                initFeedSelection = document.querySelector(".feed").innerHTML;
+        
+                loadFeed(1, function() {
+                  done();
+                });
+              });
+            });
+            it("changes its loaded content", function(done) {
+                var newFeedSelection = document.querySelector(".feed").innerHTML;
+                expect(initFeedSelection).not.toBe(newFeedSelection);
+                done();
+              });
+            });        
+
